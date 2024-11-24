2c94c0f22bd000a33edd90bff764d91a03216ef0
vitamin1312 <vitamin20021312@gmail.com>
2024-11-24 22:55:43 +0300

    added raw tasks

---
+++ scalabackend/app/controllers/TaskList.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject._
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+@Singleton
+class TaskList @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+    def taskList = Action{
+        val tasks = List("a", "b", "v")
+        Ok(views.html.taskList(tasks))
+    }
+}
