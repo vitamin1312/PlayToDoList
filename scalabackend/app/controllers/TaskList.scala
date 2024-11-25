065a5e93ef01bc6f80d7c21527c4cd9709d1a204
vitamin1312 <vitamin20021312@gmail.com>
2024-11-25 22:48:52 +0300

    Added crd operations

--- scalabackend/app/controllers/TaskList.scala
+++ scalabackend/app/controllers/TaskList.scala
@@ -17,6 +17,35 @@ class TaskList @Inject()(val controllerComponents: ControllerComponents) extends
         }.getOrElse(
             Redirect(routes.UsersController.login)
         )
+    }
 
+    def addTask = Action { implicit request =>
+
+        val usernameOption = request.session.get("username")
+        usernameOption.map { username =>
+
+            val postVals = request.body.asFormUrlEncoded
+
+            postVals.map { args =>
+                val task = args("NewTaskInput").head
+                TaskModel.addTask(username, task)
+                Redirect(routes.TaskList.taskList)
+            }.getOrElse(Redirect(routes.TaskList.taskList))
+        }.getOrElse(Redirect(routes.UsersController.login))
+    }
+
+    def deleteTask = Action { implicit request =>
+
+        val usernameOption = request.session.get("username")
+        usernameOption.map { username =>
+
+            val postVals = request.body.asFormUrlEncoded
+
+            postVals.map { args =>
+                val index = args("index").head.toInt
+                TaskModel.removeTask(username, index)
+                Redirect(routes.TaskList.taskList)
+            }.getOrElse(Redirect(routes.TaskList.taskList))
+        }.getOrElse(Redirect(routes.UsersController.login))
     }
 }
